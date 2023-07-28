9413b5e79e2e792551c12454b10a126746629309
HyeonJunSong <music9801@naver.com>
2023-07-28 15:35:41 -0700

    feat: home page UI

---
+++ lib/view/10_home_page/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:baro/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.only(top: 73.h, bottom: 38.h),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _introductionText(),
+            _wallpaperImage(),
+            _startButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _introductionText() {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 17.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Leisure goods,",
+            style: TextStyle(
+              fontSize: 32.sp,
+              fontWeight: FontWeight.bold,
+              color: colorPrimaryDark,
+            ),
+          ),
+            Text(
+            "Easy Borrow",
+            style: TextStyle(
+              fontSize: 32.sp,
+              fontWeight: FontWeight.bold,
+              color: colorPrimary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _wallpaperImage() {
+    return Container(
+      alignment: Alignment.center,
+      child: Image.asset(
+        "assets/images/home_wallpaper.png",
+        width: 317.w,
+      ),
+    );
+  }
+
+  Widget _startButton() {
+    return ElevatedButton(
+      onPressed: () {
+        // Get.toNamed("/select_place");
+      },
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(48.r),
+        ),
+        backgroundColor: colorPrimary,
+        fixedSize: Size(327.w, 48.h),
+      ),
+      child: Text(
+        "start",
+        style: TextStyle(
+          fontSize: 16.sp,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
