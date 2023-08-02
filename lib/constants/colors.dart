5f4002c7da12151196b5744b2a1ea48476965a17
HyeonJunSong <music9801@naver.com>
2023-08-01 23:23:48 -0700

    feat: color file

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,26 @@ import 'package:flutter/material.dart';
 
 const colorPrimary = Color(0xFF20D1C7);
 const colorPrimaryDark = Color(0xFF112B3C);
+
+//http://mcg.mbitson.com/
+const MaterialColor primarycolorswatch = MaterialColor(_primarycolorswatchPrimaryValue, <int, Color>{
+  50: Color(0xFFE4F9F8),
+  100: Color(0xFFBCF1EE),
+  200: Color(0xFF90E8E3),
+  300: Color(0xFF63DFD8),
+  400: Color(0xFF41D8CF),
+  500: Color(_primarycolorswatchPrimaryValue),
+  600: Color(0xFF1CCCC1),
+  700: Color(0xFF18C6BA),
+  800: Color(0xFF13C0B3),
+  900: Color(0xFF0BB5A6),
+});
+const int _primarycolorswatchPrimaryValue = 0xFF20D1C7;
+
+const MaterialColor primarycolorswatchAccent = MaterialColor(_primarycolorswatchAccentValue, <int, Color>{
+  100: Color(0xFFE1FFFC),
+  200: Color(_primarycolorswatchAccentValue),
+  400: Color(0xFF7BFFF2),
+  700: Color(0xFF62FFF0),
+});
+const int _primarycolorswatchAccentValue = 0xFFAEFFF7;
